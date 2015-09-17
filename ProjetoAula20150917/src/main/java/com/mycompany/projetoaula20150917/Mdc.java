083b7abaaccd08a9f00cbac69168984db5b65dd9
alunoinf <alunoinf@pc-649301.(none)>
2015-09-17 20:59:02 -0300

    Coloca o projeto no repositorio

---
+++ ProjetoAula20150917/src/main/java/com/mycompany/projetoaula20150917/Mdc.java
@@ -0,0 +1,34 @@
+package com.mycompany.projetoaula20150917;
+
+/**
+ * Funções matemáticas. Máximo Divisor Comum.
+ */
+public class Mdc {
+
+    /**
+     * Calcula o Máximo Divisor Comum entre 2 números.
+     * @param a Primeiro número.
+     * @param b Segundo número.
+     * @return O máximo divisor comum entre os dois números passados como parâmetro.
+     * @throws IllegalArgumentException Se o argumento "a" for menor que o 
+     * argumento "b" ou se o argumento "b" for menor ou igual a 0.
+     */
+    public static float mdc2(float a, float b) {
+
+        if (a < b) {
+            throw new IllegalArgumentException("O valor a deve ser maior ou igual ao valor de b!");
+        } else if (b <= 0) {
+            throw new IllegalArgumentException("O valor de b não pode ser menor ou igual a 0!");
+        }
+
+        while (a != b) {
+            if (a > b) {
+                a = a - b;
+            } else {
+                b = b - a;
+            }
+        }
+
+        return a;
+    }
+}
---
+++ ProjetoAula20150917/src/test/java/com/mycompany/projetoaula20150917/MdcTest.java
@@ -0,0 +1,58 @@
+package com.mycompany.projetoaula20150917;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MdcTest {
+
+    /**
+     * Teste do método mdc2 passando o parâmetro "a" menor que "b".
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void testParametroAMenorQueB() {
+        Mdc.mdc2(1, 2);
+    }
+
+    /**
+     * Teste do método mdc2 passando o parâmetro "b" igual a 0.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void testParametroBIgualA0() {
+        Mdc.mdc2(1, 0);
+    }
+
+    /**
+     * Teste do método mdc2 passando o parâmetro "b" menor que 0.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void testParametroBMenorQue0() {
+        Mdc.mdc2(1, -2);
+    }
+
+    /**
+     * Teste do método mdc2 passando os parâmetros iguais e esperando 
+     * o retorno 1.
+     */
+    @Test
+    public void testEspera1() {
+        Assert.assertTrue((float) 1 == Mdc.mdc2(1, 1));
+    }
+
+    /**
+     * Teste do método mdc2 passando dois parâmetros diferentes e esperando 
+     * o retorno 5.
+     */
+    @Test
+    public void testEspera5() {
+        Assert.assertTrue((float) 5 == Mdc.mdc2(10, 5));
+    }
+
+    /**
+     * Teste do método mdc2 passando dois parâmetros diferentes e esperando 
+     * o retorno 4.
+     */
+    @Test
+    public void testEspera4() {
+        Assert.assertTrue((float) 4 == Mdc.mdc2(12, 8));
+    }
+}
