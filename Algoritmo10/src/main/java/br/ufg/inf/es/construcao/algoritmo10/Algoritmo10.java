df5e8ad8e3c610f81cecc2faacb6a43fe3146ae7
Felipe Kuninry Akitaya <felipeakitaya@gmail.com>
2015-11-11 07:02:50 -0200

    Revert "Implentação, teste e correções - Algoritmos 1~10"
    This reverts commit 295556479e2703c4c55d5067d7702776b015225e.

--- Algoritmo10/src/main/java/br/ufg/inf/es/construcao/algoritmo10/Algoritmo10.java
+++ Algoritmo10/src/main/java/br/ufg/inf/es/construcao/algoritmo10/Algoritmo10.java
@@ -2,46 +2,26 @@ package br.ufg.inf.es.construcao.algoritmo10;
 
 public class Algoritmo10 {
 
-    /**
-     * Dado um vetor e uma posição, atribui o valor 1 às posições que tem o
-     * valor não primo.
-     *
-     * @param a É o vetor a ser modificado.
-     * @param n É o limitador de verificação e atribuição.
-     *
-     * @throws IllegalArgumentException Caso o valor de 'n' for menor ou igual a
-     * 1. Caso o valor de 'n' + 1 supere o número de posições do vetor
-     * informado. Caso para 2<=i<=n o valro da posição n não for 0.
-     */
-    public static void crivoEratostenes(int[] a, int n) {
+    public static void CrivoEratostenes(int[] a, int n) {
+
         if (n <= 1) {
-            throw new IllegalArgumentException("O valor de 'n' deve ser maior "
-                    + "que 1!");
+            throw new IllegalArgumentException("O valor de n deve ser maior que 1!");
         }
+        
+        int i;
 
-        if (a.length < (n + 1)) {
-            throw new IllegalArgumentException("Tamanho vetor não válido!");
-        }
-
-        for (int i = 2; i <= n; i++) {
-            if (a[i] != 0) {
-                throw new IllegalArgumentException("Para todo 2<=i<=n a[i] "
-                        + "deve ser igual a 0!");
-            }
-        }
-
-        int i = 2;
-        int c = 0;
-        while (i <= n / 2) {
+        i = 2;
+        while (i <= (n / 2)) {
             if (a[i] == 0) {
-                c = i + i;
+                int c = i + i;
                 while (c <= n) {
                     a[c] = 1;
-                    c += i;
+                    c = c + i;
                 }
             }
-            i += 1;
+            i = i + 1;
         }
+
     }
 
 }
